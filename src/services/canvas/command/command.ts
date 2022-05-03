c83edfdaae167516bf710dcaea0469dd6f7a88d6
Simon Martineau <simon.martineau.3@ulaval.ca>
2022-05-03 17:22:53 -0400

    forgot to commit this

--- src/services/canvas/command/command.ts
+++ src/services/canvas/command/command.ts
@@ -1,21 +1,3 @@
 export abstract class Command {
   abstract execute(context: CanvasRenderingContext2D): void;
 }
-
-export class CommandGroup {
-  private commands: Command[] = [];
-
-  addCommand(command: Command) {
-    this.commands.push(command);
-  }
-
-  executeAll(context: CanvasRenderingContext2D) {
-    this.commands.forEach((command) => {
-      command.execute(context);
-    });
-  }
-
-  empty() {
-    return this.commands.length === 0;
-  }
-}
